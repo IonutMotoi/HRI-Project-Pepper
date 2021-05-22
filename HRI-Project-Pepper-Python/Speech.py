7b1eb618b0fd34e6acae58103f02e32bae273f53
Lio320 <saracenileonardo01@gmail.com>
2021-05-22 16:15:31 +0200

    Speech recognition, basic

--- HRI-Project-Pepper-Python/Speech.py
+++ HRI-Project-Pepper-Python/Speech.py
@@ -3,10 +3,19 @@ import speech_recognition as sr
 
 r = sr.Recognizer()
 with sr.Microphone() as source:
+    #r.adjust_for_ambient_noise(source, duration=1)
+    r.energy_threshold = 300
     print("Speak Anything :")
     audio = r.listen(source)
+    
     try:
-        text = r.recognize_google(audio)
-        print("You said : {}".format(text))
+        text = r.recognize_google(audio, language="en-US")
+        print("You said (US) : {}".format(text))
+    except:
+        print("Sorry could not recognize what you said")
+
+    try:
+        text = r.recognize_google(audio, language="en-GB")
+        print("You said (GB) : {}".format(text))
     except:
         print("Sorry could not recognize what you said")
