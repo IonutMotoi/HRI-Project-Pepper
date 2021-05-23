a7616e99e413860d50fa207faa0d19204d0be67c
Ionut Motoi <32934655+IonutMotoi@users.noreply.github.com>
2021-05-23 16:30:46 +0200

    QiChatbot, start menu activity

--- HRI-Project-Pepper-Android/app/src/main/java/com/example/myapplication/MainActivity.java
+++ HRI-Project-Pepper-Android/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,17 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.aldebaran.qi.Future;
 import com.aldebaran.qi.sdk.QiContext;
 import com.aldebaran.qi.sdk.QiSDK;
 import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
+import com.aldebaran.qi.sdk.builder.ChatBuilder;
+import com.aldebaran.qi.sdk.builder.QiChatbotBuilder;
 import com.aldebaran.qi.sdk.builder.SayBuilder;
+import com.aldebaran.qi.sdk.builder.TopicBuilder;
 import com.aldebaran.qi.sdk.design.activity.RobotActivity;
+import com.aldebaran.qi.sdk.object.conversation.Chat;
+import com.aldebaran.qi.sdk.object.conversation.QiChatVariable;
+import com.aldebaran.qi.sdk.object.conversation.QiChatbot;
 import com.aldebaran.qi.sdk.object.conversation.Say;
+import com.aldebaran.qi.sdk.object.conversation.Topic;
 
 public class MainActivity  extends RobotActivity implements RobotLifecycleCallbacks {
+
+    private static final String TAG ="MainActivty";
+
+    // The QiContext provided by the QiSDK.
+    private QiContext qiContext = null;
+
+    // Store the Chat action.
+    private Chat chatAction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,18 +51,27 @@ public class MainActivity  extends RobotActivity implements RobotLifecycleCallba
 
     @Override
     public void onRobotFocusGained(QiContext qiContext) {
-        // Create a new say action.
-        Say say = SayBuilder.with(qiContext) // Create the builder with the context.
-                .withText("Hello human!") // Set the text to say.
-                .build(); // Build the say action.
+        // Store the provided QiContext.
+        this.qiContext = qiContext;
+        initActions();
 
-        // Execute the action.
-        say.run();
+        // Run the Chat action asynchronously.
+        Future<Void> chatFuture = chatAction.async().run();
+
+        // Add a lambda to the action execution.
+        chatFuture.thenConsume(future -> {
+            if (future.hasError()) {
+                Log.e(TAG, "Discussion finished with error.", future.getError());
+            }
+        });
     }
 
     @Override
     public void onRobotFocusLost() {
-        // Nothing here.
+        // Remove on started listeners from the Chat action.
+        if (chatAction != null) {
+            chatAction.removeAllOnStartedListeners();
+        }
     }
 
     @Override
@@ -57,5 +83,43 @@ public class MainActivity  extends RobotActivity implements RobotLifecycleCallba
     public void startOrder(View view) {
         Intent intent = new Intent(this, MenuActivity.class);
         startActivity(intent);
+    }
+
+    /** Called when the user says Start Order */
+    public void startOrder() {
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
+    }
+
+    public void initActions() {
+        // Create a chat topic
+        Topic topic = TopicBuilder.with(qiContext) // Create the builder using the QiContext.
+                .withResource(R.raw.greetings) // Set the topic resource.
+                .build(); // Build the topic.
+
+        // Create a new QiChatbot.
+        QiChatbot qiChatbot = QiChatbotBuilder.with(qiContext)
+                .withTopic(topic)
+                .build();
+
+        // Create a new Chat action.
+        chatAction = ChatBuilder.with(qiContext)
+                .withChatbot(qiChatbot)
+                .build();
+
+        // Add an on started listener to the Chat action.
+        chatAction.addOnStartedListener(() -> Log.i(TAG, "Discussion started."));
+
+        // Set up a listener for a chat variable
+        QiChatVariable startVariable = qiChatbot.variable("Start");
+
+        startVariable.addOnValueChangedListener(
+                currentValue -> {
+                    if (currentValue == "true") {
+                        Log.i(TAG, "Chat var Start: " + currentValue);
+                        startOrder();
+                    }
+                }
+        );
     }
 }
