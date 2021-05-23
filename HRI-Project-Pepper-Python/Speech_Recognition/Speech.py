a57ef78cfee2958fae6b782c8075ccb746bdf034
Ionut Motoi <32934655+IonutMotoi@users.noreply.github.com>
2021-05-23 13:58:27 +0200

    Update Speech.py -> continuous listening, pause after recognition

--- HRI-Project-Pepper-Python/Speech_Recognition/Speech.py
+++ HRI-Project-Pepper-Python/Speech_Recognition/Speech.py
@@ -4,26 +4,36 @@ from pywinauto import Desktop
 from pywinauto.application import Application
 import time 
 
+# Initialize speech recognition
 r = sr.Recognizer()
 
-with sr.Microphone(device_index=1) as source:
-    #r.adjust_for_ambient_noise(source, duration=1)
-    #r.energy_threshold = 300
-    print("Speak Anything :")
-    audio = r.listen(source)
-    
-    try:
-        text = r.recognize_google(audio, language="en-GB")
-        print("You said (US) : {}".format(text))
-    except:
-        print("Sorry could not recognize what you said")
-
-###WRITE TEXT
-#keyboard.write(text)
-
+# Initialize "Dialog view" window controller
 app = Application().connect(title='Dialog view')
 more = app.DialogView
 more.wrapper_object()
-more.set_focus()
-time.sleep(1)
-keyboard.write(text)
+
+robot_is_speaking = False
+
+while(True):
+    if not robot_is_speaking:
+        with sr.Microphone(device_index=1) as source:
+            #r.adjust_for_ambient_noise(source, duration=1)
+            #r.energy_threshold = 300
+            print("Speak Anything :")
+            audio = r.listen(source)
+            
+            try:
+                text = r.recognize_google(audio, language="en-US")
+                print("You said (US) : {}".format(text))
+                
+                more.set_focus()
+                time.sleep(0.1)
+                keyboard.write(text)
+                keyboard.press('enter')
+
+                robot_is_speaking = True
+            except:
+                print("Sorry could not recognize what you said")
+    else:
+        time.sleep(3)
+        robot_is_speaking = False
