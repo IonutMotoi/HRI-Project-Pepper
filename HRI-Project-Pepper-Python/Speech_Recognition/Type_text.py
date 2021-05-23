3e1504e286a51af9197b24ea742cfcee8a3ae18f
Lio320 <saracenileonardo01@gmail.com>
2021-05-23 12:52:29 +0200

    Update Type_text.py

--- HRI-Project-Pepper-Python/Speech_Recognition/Type_text.py
+++ HRI-Project-Pepper-Python/Speech_Recognition/Type_text.py
@@ -1,3 +1,23 @@
 import keyboard
+from pywinauto import Desktop
+from pywinauto.application import Application
 
-keyboard.write('The quick brown fox jumps over the lazy dog.')
+###WRITE TEXT
+#keyboard.write(text)
+
+###GET ALL OPENED WINDOWS NAME
+
+# windows = Desktop(backend="uia").windows()
+# print([w.window_text() for w in windows])
+
+#app = Application(backend='uia').start("C:/Program Files/Android/Android Studio/bin/studio64.exe").connect(title='My Application – robotsdk.xml [My_Application.app]')
+#app = Application(backend='win32').connect(title='Robot Viewer')
+app = Application().connect(title='Robot Viewer')
+more = app.RobotViewer
+more.wrapper_object()
+more.set_focus()
+#app.RobotViewer.menu_select('Display 3d information')
+# dlg_spec = app.RobotViewerapp.FreeAgent.menu_select("File->Import and Export->Export Address Book")
+# actionable_dlg = dlg_spec.wait('visible')
+
+
